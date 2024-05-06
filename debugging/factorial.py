1c29beeaebda58d9dcb0ff175f437490bc67a716
Stefani-web <stephaniecvlho@gmail.com>
2024-05-06 12:27:19 +0200

    Fix debbug

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,8 +5,23 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n = n - 1
+        n -= 1 # Décrémenter n à chaque itération
     return result
 
-f = factorial(int(sys.argv[1]))
+
+if len(sys.argv) != 2:
+    print("Usage: python script.py <number>")
+    sys.exit(1)
+
+try:
+    number = int(sys.argv[1])
+except ValueError:
+    print("Erreur : L'argument doit être un nombre entier.")
+    sys.exit(1)
+
+if number < 0:
+    print("Erreur : Le nombre doit être positif.")
+    sys.exit(1)
+
+f = factorial(number)
 print(f)
